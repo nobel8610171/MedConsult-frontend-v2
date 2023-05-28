42ec79a81f930f0fd808b620801464a3b58c303c
黃冠博 <xingjuan@huangguanbodeMacBook-Pro.local>
2023-05-28 16:48:05 +0800

    Updates

---
+++ precache-manifest.29a4351b9ebc7611dff61fef4bff8cfc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be7f798fb5e890d0b81",
+    "url": "/MedConsult-frontend-v2/static/css/main.c6ca6942.chunk.css"
+  },
+  {
+    "revision": "6be7f798fb5e890d0b81",
+    "url": "/MedConsult-frontend-v2/static/js/main.6be7f798.chunk.js"
+  },
+  {
+    "revision": "676c8d3e0fc45ad1eec8",
+    "url": "/MedConsult-frontend-v2/static/js/1.676c8d3e.chunk.js"
+  },
+  {
+    "revision": "17f68a15228cf711e2a9",
+    "url": "/MedConsult-frontend-v2/static/js/2.17f68a15.chunk.js"
+  },
+  {
+    "revision": "d82778110e696c56d2e3",
+    "url": "/MedConsult-frontend-v2/static/js/runtime~main.d8277811.js"
+  },
+  {
+    "revision": "5ef5f772b0579320f5a789ab8cb39dd2",
+    "url": "/MedConsult-frontend-v2/static/media/browser.5ef5f772.cjs"
+  },
+  {
+    "revision": "3258e6ca43454ab432444aefbb50dcac",
+    "url": "/MedConsult-frontend-v2/static/media/old.3258e6ca.png"
+  },
+  {
+    "revision": "441f1ac2074702ae7e1c6dc10761114e",
+    "url": "/MedConsult-frontend-v2/static/media/chatgpt.441f1ac2.png"
+  },
+  {
+    "revision": "e11530ea2c6669a9971acbf469c6d519",
+    "url": "/MedConsult-frontend-v2/index.html"
+  }
+];
